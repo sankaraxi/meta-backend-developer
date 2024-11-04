8dde13caf00db47da57ba2ad185177bf28501d5c
sankaraxi <sankarfrompalani@gmail.com>
2024-11-04 17:46:49 +0530

    Add data_types.py with examples of Python data types and their usage

---
+++ Python Programming/data_types.py
@@ -0,0 +1,116 @@
+# Types of data in Python
+
+# 1. Numbers
+# 2. Strings
+# 3. List
+# 4. Tuple
+# 5. Dictionary
+# 6. Set
+# 7. Boolean
+
+# Numbers
+# 1. int
+# 2. float
+# 3. complex
+# 4. bool
+
+#examples
+# int
+x = 1
+print(x)
+print(type(x))
+#output: type of x is <class 'int'>
+
+# float
+x = 1.0
+print(x)
+print(type(x))
+#output: type of x is <class 'float'>
+
+# complex
+x = 1 + 2j
+print(x)
+print(type(x))
+#output: type of x is <class 'complex'>
+
+# bool
+x = True
+print(x)
+print(type(x))
+#output: type of x is <class 'bool'>
+
+
+
+# Strings
+# 1. single line string
+# 2. multi line string
+# 3. string formatting
+
+
+# Examples
+
+# single line string
+x = 'Hello World'
+print(x)
+print(type(x))
+#output: type of x is <class 'str'>
+
+# multi line string
+x = '''Hello
+World'''
+print(x)
+print(type(x))
+#output: type of x is <class 'str'>
+
+# string formatting
+name = 'Sankar'
+age = 21
+x = 'My name is {} and I am {} years old'.format(name, age) #using format method
+print(x)
+#output: My name is Sankar and I am 21 years old
+
+# List
+# 1. list
+# 2. list methods
+# 3. list comprehension
+
+#examples
+# list
+x = [1, 2, 3, 4]
+print(x)
+print(type(x))
+#output: type of x is <class 'list'>
+
+# Tuple
+# 1. tuple
+# 2. tuple methods
+
+#examples
+# tuple
+x = (1, 2, 3, 4)
+print(x)
+print(type(x))
+#output: type of x is <class 'tuple'>
+
+# Dictionary
+# 1. dictionary
+# 2. dictionary methods
+
+#examples
+# dictionary
+x = {'name': 'Sankar', 'age': 21}
+print(x)
+print(type(x))
+#output: type of x is <class 'dict'>
+
+# Set
+# 1. set
+# 2. set methods
+
+#examples
+# set
+x = {1, 2, 3, 4}
+print(x)
+print(type(x))
+#output: type of x is <class 'set'>
+
