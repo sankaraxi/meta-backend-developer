69d9e506c6eb4c13227a4aed23ed3d8352c64317
sankaraxi <sankarfrompalani@gmail.com>
2024-11-15 20:50:05 +0530

    Add secondApp with initial views, URLs, and configuration

--- Django Web Framework/FirstDjangoWebApp/firstDjango/firstDjango/urls.py
+++ Django Web Framework/FirstDjangoWebApp/firstDjango/firstDjango/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [ 
     path('', include('firstApp.urls')), 
     path('admin/', admin.site.urls), 
+    path('home/', include('secondApp.urls')),
 ] 
